dcc8d9970e97c3c005bf3002e37bc637b03b1477
ciaociao <misterkome0514@yahoo.co.jp>
2020-06-13 22:16:18 +0000

    jun14  commit

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,6 @@ Route::get("XXX", "Admin\AAAController@bbb");
 //ProfileController の add Action に、admin/profile/edit にアクセスしたら 
 //ProfileController の edit Action に割り当てるように設定してください。
 
-Route::get("admin/profile/create","Admin\ProfileController@add");
-
-Route::get("admin/profile/edit","Admin\ProfileController@edit");
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -41,13 +37,14 @@ Route::group(["prefix" => "admin"], function() {
     Route::get("news/create","Admin\NewsController@add")->middleware("auth");
 });
 
-Route::group(["prefix" => "profile"], function() {
+Route::group(["prefix" => "admin"], function() {
     Route::get("profile/create","Admin\ProfileController@add")->middleware("auth");
-    Route::post('profile/create', 'Admin\ProfileController@create');
+
+    Route::get("profile/edit","Admin\ProfileController@edit")->middleware("auth");
+
+
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-     Route::get('news/create', 'Admin\NewsController@add');
-     Route::post('news/create', 'Admin\NewsController@create');
-});
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
